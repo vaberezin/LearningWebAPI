ca5503ac552b34d2b77142604c52e3a485a6c990
Vlad Berezin <vaberezin@protonmail.com>
2019-11-19 23:45:03 +0300

    In progress for 37 minutes Corey channel on YT

---
+++ WebAPIDemoApp/DemoAPI/Controllers/PeopleController.cs
@@ -0,0 +1,49 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoAPI.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        // GET: api/People
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add ( new Person { FirstName = "Vlad", LastName = "Berezin", Id = 1});
+            people.Add(new Person { FirstName = "Alyona", LastName = "Varankina", Id = 2 });
+            people.Add(new Person { FirstName = "Clark", LastName = "Kent", Id = 3   });
+        }
+
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/People
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
